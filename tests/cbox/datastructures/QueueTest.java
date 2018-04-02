1b7e2b4442c296863b4ec3485ed216d3d1e6c4f7
MAShah-UK <m.a.shah@live.co.uk>
2018-04-02 23:12:23 +0100

    Wrote test cases for Queue.

--- tests/cbox/datastructures/QueueTest.java
+++ tests/cbox/datastructures/QueueTest.java
@@ -1,6 +1,64 @@
 package cbox.datastructures;
 
-public class QueueTest {
-    Queue<Integer> queue;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class QueueTest {
+    Queue<Integer> q;
+
+    @Test
+    public void isEmptyTrue() {
+        q = new Queue<>();
+        Assert.assertTrue(q.isEmpty());
+    }
+
+    @Test
+    public void isEmptyTrue2() {
+        q = new Queue<>();
+        q.push(1);
+        q.pop();
+        Assert.assertTrue(q.isEmpty());
+    }
+
+    @Test
+    public void isEmptyFalse() {
+        q = new Queue<>();
+        q.push(1);
+        Assert.assertFalse(q.isEmpty());
+    }
+
+    @Test
+    public void peakPop() {
+        q = new Queue<>();
+        q.push(1);
+        Assert.assertEquals((Integer) 1, q.peak());
+        Assert.assertEquals((Integer) 1, q.pop());
+    }
+
+    @Test
+    public void pushPopOneElement() {
+        q = new Queue<>();
+        q.push(1);
+        Assert.assertEquals((Integer) 1, q.pop());
+    }
+
+    @Test
+    public void pushPopTwoElements() {
+        q = new Queue<>();
+        q.push(1);
+        q.push(2);
+        Assert.assertEquals((Integer) 1, q.pop());
+        Assert.assertEquals((Integer) 2, q.pop());
+    }
+
+    @Test
+    public void pushPopThreeElements() {
+        q = new Queue<>();
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        Assert.assertEquals((Integer) 1, q.pop());
+        Assert.assertEquals((Integer) 2, q.pop());
+        Assert.assertEquals((Integer) 3, q.pop());
+    }
 }
