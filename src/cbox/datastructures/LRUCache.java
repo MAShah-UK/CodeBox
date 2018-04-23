ff5690b7ce90adfcf116bb493c7a826c98f9a363
MAShah-UK <m.a.shah@live.co.uk>
2018-04-23 20:48:37 +0100

    Implemented a least recently used cache using LinkedHashMap.

---
+++ src/cbox/datastructures/LRUCache.java
@@ -0,0 +1,20 @@
+package cbox.datastructures;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// Least Recently Used Cache.
+// Contains a fixed set of values, removes the oldest first is size is exceeded.
+public class LRUCache<T, U> extends LinkedHashMap<T, U> {
+    private int size;
+
+    @Override
+    private boolean removeEldestEntry(Map.Entry eldest) {
+        return size() > size;
+    }
+
+    public LRUCache(int size) {
+        super(size, 0.75f, true);
+        this.size = size;
+    }
+}
